be303ba3752eef380047b68f7e1181ac595af289
Jorge Alves <jorgeedualves84@gmail.com>
2022-06-13 23:06:29 -0300

    include philosophers.h

--- include/philosophers.h
+++ include/philosophers.h
@@ -1,4 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joeduard <joeduard@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/13 22:12:02 by joeduard          #+#    #+#             */
+/*   Updated: 2022/06/13 23:05:30 by joeduard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+
 #include <stdlib.h>
-#include <stdio.h>>
+#include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
+
+
+#endif
