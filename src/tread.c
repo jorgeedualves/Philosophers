5553765556d237b7b2a50524f726334617105cd9
Jorge Alves <jorgeedualves84@gmail.com>
2022-06-21 18:50:18 -0300

    testes de thread

--- src/tread.c
+++ src/tread.c
@@ -5,7 +5,15 @@ void* routine(){
 }
 int main(int argc, char*argv[]){
     pthread_t t1;
-    pthread_create(&t1, NULL, &routine, NULL);
+    pthread_t t2;
 
+    if (pthread_create(&t1, NULL, &routine, NULL) != 0)
+        return(1);
+    if (pthread_create(&t2, NULL, &routine, NULL) != 0)
+        return(2);
+    if(pthread_join(t1, NULL) != 0);
+       return(3);
+   if(pthread_join(t1, NULL) != 0);
+       return(4);
     return (0);
 }
