f6f3f8da74482d1a925da4fbc4b97cb745ca9bb5
Jorge Alves <jorgeedualves84@gmail.com>
2022-06-23 13:51:03 -0300

    teste barries

--- src/main_barries.c
+++ src/main_barries.c
@@ -17,15 +17,15 @@ void* routine(void* args) {
 }
 
 int main(int argc, char *argv[]) {
-	pthread_t th[10];
+	pthread_t th[3];
 	int i;
-    pthread_barrier_init(&barrier, NULL, 10);
-	for (i = 0; i < 10; i++) {
+    pthread_barrier_init(&barrier, NULL, 3);
+	for (i = 0; i < 3; i++) {
 		if (pthread_create(&th[i], NULL, &routine, NULL) != 0) {
 			perror("Failed to create thread");
 		}
 	}
-	for (i = 0; i < 10; i++) {
+	for (i = 0; i < 3; i++) {
 		if (pthread_join(th[i], NULL) != 0) {
 			perror("Failed to join thread");
 		}
